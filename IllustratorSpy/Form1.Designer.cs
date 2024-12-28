8c766d450422f74cbf3d6ee8ad0bdcc13f6e0c78
Sam Huang <samhng@hotmail.com>
2024-12-29 03:23:29 +0800

    added basic code.

--- IllustratorSpy/Form1.Designer.cs
+++ IllustratorSpy/Form1.Designer.cs
@@ -28,13 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this.treeView1 = new System.Windows.Forms.TreeView();
+      this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.menuStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel1.SuspendLayout();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+      this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+      this.menuStrip1.Size = new System.Drawing.Size(816, 25);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+      this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+      this.splitContainer1.Name = "splitContainer1";
+      // 
+      // splitContainer1.Panel1
+      // 
+      this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+      // 
+      // splitContainer1.Panel2
+      // 
+      this.splitContainer1.Panel2.Controls.Add(this.propertyGrid1);
+      this.splitContainer1.Size = new System.Drawing.Size(816, 574);
+      this.splitContainer1.SplitterDistance = 271;
+      this.splitContainer1.SplitterWidth = 5;
+      this.splitContainer1.TabIndex = 1;
+      // 
+      // treeView1
+      // 
+      this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.treeView1.Location = new System.Drawing.Point(0, 0);
+      this.treeView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+      this.treeView1.Name = "treeView1";
+      this.treeView1.Size = new System.Drawing.Size(271, 574);
+      this.treeView1.TabIndex = 0;
+      // 
+      // propertyGrid1
+      // 
+      this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.propertyGrid1.Location = new System.Drawing.Point(0, 0);
+      this.propertyGrid1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+      this.propertyGrid1.Name = "propertyGrid1";
+      this.propertyGrid1.Size = new System.Drawing.Size(540, 574);
+      this.propertyGrid1.TabIndex = 0;
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+      this.fileToolStripMenuItem.Text = "&File";
+      // 
+      // openToolStripMenuItem
+      // 
+      this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+      this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.openToolStripMenuItem.Text = "&Open";
+      this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(816, 599);
+      this.Controls.Add(this.splitContainer1);
+      this.Controls.Add(this.menuStrip1);
+      this.Font = new System.Drawing.Font("CozetteVector", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.MainMenuStrip = this.menuStrip1;
+      this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.splitContainer1.Panel1.ResumeLayout(false);
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
         }
 
-        #endregion
-    }
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.TreeView treeView1;
+    private System.Windows.Forms.PropertyGrid propertyGrid1;
+  }
 }
 
--- IllustratorSpy/Form1.cs
+++ IllustratorSpy/Form1.cs
@@ -7,14 +7,129 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Illustrator;
+using Application = Illustrator.Application;
 
 namespace IllustratorSpy
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+    private Application _app;
+
+    private Illustrator.Document _doc;
+
+    public Form1()
         {
             InitializeComponent();
+
+      treeView1.Click += TreeView1_Click;
         }
+
+    private void TreeView1_Click(object sender, EventArgs e)
+    {
+      switch(treeView1.SelectedNode.ToolTipText)
+      {
+        case "Document":
+          Document document = treeView1.SelectedNode.Tag as Document;
+          propertyGrid1.SelectedObject = document;
+          break;
+        case "Layer":
+          Layer layer = treeView1.SelectedNode.Tag as Layer;
+          propertyGrid1.SelectedObject = layer;
+          break;
+        case "Group":
+          GroupItem groupItem = treeView1.SelectedNode.Tag as GroupItem;
+          propertyGrid1.SelectedObject = groupItem;
+          break;
+        case "TextFrame":
+          TextFrame textFrame= treeView1.SelectedNode.Tag as TextFrame;
+          propertyGrid1.SelectedObject = textFrame;
+          break;
+        case "TextRange":
+          TextRange textRange = treeView1.SelectedNode.Tag as TextRange;
+          propertyGrid1.SelectedObject = textRange;
+          break;
+      }
     }
+
+    private void openToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      var d = new OpenFileDialog();
+      if (d.ShowDialog() == DialogResult.OK)
+      {
+        _app = new Application();
+        _app.UserInteractionLevel = AiUserInteractionLevel.aiDontDisplayAlerts;
+        _doc = _app.Open(d.FileName);
+        treeView1.Nodes.Clear();
+        var docNode = treeView1.Nodes.Add(_doc.Name);
+        Visit(_doc, docNode);
+      }
+    }
+
+    public void Visit(Document document, TreeNode node)
+    {
+      node.Tag = document;
+      node.Text = document.Name;
+      node.ToolTipText = "Document";
+      foreach(Layer layer in document.Layers)
+      {
+        var n = node.Nodes.Add(layer.Name);
+        Visit(layer, n);
+      }
+    }
+
+    public void Visit(Layer layer, TreeNode node)
+    {
+      foreach(GroupItem groupItem in layer.GroupItems)
+      {
+        var n = node.Nodes.Add(groupItem.Name);
+        Visit(groupItem, n);
+      }
+      foreach(TextFrame textFrame in layer.TextFrames)
+      {
+        var n = node.Nodes.Add(textFrame.Name);
+        Visit(textFrame, n);
+      }
+    }
+
+    public void Visit(TextFrame textFrame, TreeNode node)
+    {
+      node.Tag = textFrame;
+      node.Text = textFrame.Name;
+      node.ToolTipText = "TextFrame";
+      foreach(TextRange textRange in textFrame.TextRanges)
+      {
+        var n = node.Nodes.Add(textRange.Contents);
+        Visit(textRange, node);
+      }
+    }
+
+    public void Visit(TextRange textRange, TreeNode node)
+    {
+      node.Tag = textRange;
+      node.Text = textRange.Contents;
+      node.ToolTipText = "TextRange";
+      foreach(TextRange range in textRange.TextRanges)
+      {
+        var n = node.Nodes.Add(range.Contents);
+        Visit(range, node);
+      }
+    }
+
+
+    public bool Visit(GroupItem group, TreeNode node)
+    {
+      foreach (TextFrame textFrame in group.TextFrames)
+      {
+        Visit(textFrame, node);
+      }
+      foreach (GroupItem groupItem in group.GroupItems)
+      {
+        Visit(groupItem, node);
+      }
+
+      return true;
+    }
+  }
 }
